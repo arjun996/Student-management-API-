2abf28b85eb31bb128c2408c184b873b51d2e55f
arjun996 <arjunmadikai996@gmail.com>
2023-07-18 13:08:31 +0530

    AbUserdetails add

--- student_management_system/api/models.py
+++ student_management_system/api/models.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # Additional fields if needed
-    pass
+    first_name    = models.CharField(max_length=50)
+    last_name     = models.CharField(max_length=50)
+    username      = models.CharField(max_length=50, unique=True)
+    email         = models.EmailField(max_length=100, unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
+    def __str__(self):
+        return self.username
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
